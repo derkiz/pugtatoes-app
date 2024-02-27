cfb27169a68f8e607310cd39d0e536c34e3153dd
derkiz <aconejulien@gmail.com>
2024-02-27 11:22:16 +0100

    pre cta fix

--- src/components/Latest.tsx
+++ src/components/Latest.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './latest.css'
+import Link from 'next/link'
 
 const Latest = () => {
   return (
     <>
       <div className="latest-parent">
-        <div className="latest-section">Latest</div>
+        <div className="latest">
+          <h2>Meet our bestsellers</h2>
+          <div className='best-products'>
+            <ul>
+              <li><Link href="slug">product 1</Link></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   )
