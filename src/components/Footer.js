c446a23fae41e51512b0e30f4351ea6436041e63
derkiz <aconejulien@gmail.com>
2024-03-22 14:58:33 +0100

    added module.css compatibility for footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import './footer.css'
+import styles from './Footer.module.css'
 import Link from 'next/link'
 
 const Footer = () => {
   return (
   <>
-    <div className="footer">
-      <div className="footer-flex-container">
-        <div className="footer-column">
-          <div className="footer-title">Quick Links</div>
+    <div className={styles.footer}>
+      <div className={styles.footer_flex_container}>
+        <div className={styles.footer_column}>
+          <div className={styles.footer_title}>Quick Links</div>
             <ul>
               <Link href="/collections" style={{ textDecoration: "none", color: "inherit" }}><li>Collections</li></Link>
               <Link href="/pages/about" style={{ textDecoration: "none", color: "inherit" }}><li>About</li></Link>
@@ -18,35 +18,35 @@ const Footer = () => {
               <Link href="/pages/terms-and-conditions" style={{ textDecoration: "none", color: "inherit" }}><li>Terms and Conditions</li></Link>
             </ul>
           </div>
-        <div className="footer-column">
-          <div className="footer-title">Our mission</div>
+        <div className={styles.footer_column}>
+          <div className={styles.footer_title}>Our mission</div>
             <p>Our aim is to create the cutest pug art products with our talented artist Zoe to bring joy to all of our customers! </p>
         </div>
-        <div className="mobile-menu-icon-3">
-          <div className="footer-image-holder">
+        <div className={styles.mobile_menu_icon_3}>
+          <div className={styles.footer_image_holder}>
             <img src="/static/pugtatoes-logo.svg"></img>
           </div>
         </div>
       </div>
-      <div className="footer-flex-container">
-        <div className="newsletter">
-          <div className="footer-title">Stay updated!</div>
-          <div className="insert-email">
+      <div className={styles.footer_flex_container}>
+        <div className={styles.newsletter}>
+          <div className={styles.footer_title}>Stay updated!</div>
+          <div className={styles.insert_email}>
             <p>Email</p>
-            <div className="email-icon">
+            <div className={styles.email_icon}>
               <img src="/static/arrow-right.svg" alt="arrow-right svg"></img>
             </div>
           </div>
         </div>
-        <div className="socials">
-          <div className="social-item">
-            <img className="footer-icon" src="/static/instagram.svg" alt="Instagram Icon"></img>
+        <div className={styles.socials}>
+          <div className={styles.social_item}>
+            <img className={styles.footer_icon} src="/static/instagram.svg" alt="Instagram Icon"></img>
           </div>
-          <div className="social-item">
-            <img className="footer-icon-2" src="/static/x.svg" alt="Instagram Icon"></img>
+          <div className={styles.social_item}>
+            <img className={styles.footer_icon_2} src="/static/x.svg" alt="Instagram Icon"></img>
           </div>
-          <div className="social-item">
-            <img className="footer-icon" src="/static/tiktok.svg" alt="Instagram Icon"></img>
+          <div className={styles.social_item}>
+            <img className={styles.footer_icon} src="/static/tiktok.svg" alt="Instagram Icon"></img>
           </div>
         </div>
       </div>
