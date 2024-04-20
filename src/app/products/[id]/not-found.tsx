2828ba4ec5a79ec3de68905417dd1649becb1111
derkiz <aconejulien@gmail.com>
2024-04-20 11:59:07 +0200

    adds slug array and error handling

---
+++ src/app/products/[id]/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <>
+      <h2>Product not found</h2>
+      <p>Could not find requested response</p>
+    </>
+  )
+}
+
+export default NotFound
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,9 +1,11 @@
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
+
 type Props = {
   params: {
     id: string;
-  }
-}
+  };
+};
 
 export const generateMetadata = ({ params }: Props): Metadata => {
   return {
@@ -11,8 +13,30 @@ export const generateMetadata = ({ params }: Props): Metadata => {
   };
 };
 
-export default function productDetails({ 
-  params,
-}: Props) {
-  return <div>Product Details: { params.id }</div>
+const ProductDetails = async ({ params }: Props) => {
+  try {
+    const response = await fetch("http://localhost:1337/api/products");
+    const { data } = await response.json();
+    const slugs = data.map((product: { attributes: { slug: any } }) => product.attributes.slug);
+
+    if (!slugs.includes(params.id)) {
+      console.log(`ID "${params.id}" not found in slugs array`); // Log a message
+      notFound();
+    }
+
+    return (
+      <div>
+        Product Details: {params.id}
+        <br />
+        Slugs array: {JSON.stringify(slugs)}
+      </div>
+    );
+    
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+export default function ProductPage({ params }: Props) {
+  return <ProductDetails params={params} />;
 }
