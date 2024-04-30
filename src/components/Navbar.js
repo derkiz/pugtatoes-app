c8b14f88027db187e8cfe296fa76f80ab16ae422
derkiz <aconejulien@gmail.com>
2024-04-30 17:14:01 +0200

    cleans styling navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import styles from './Navbar.module.css'
-import Link from 'next/link'
+import React from 'react';
+import styles from './Navbar.module.css';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
@@ -8,27 +8,43 @@ const Navbar = () => {
       <div className={styles.nav}>
         <div className={styles.nav_container}>
           <div className={styles.mobile_menu_icon}>
-            <img className={styles.list_svg} src="/static/list.svg" alt="menu icon"></img>
+            <img className={styles.list_svg} src="/static/list.svg" alt="menu icon" />
           </div>
-          <Link href='/'><img className={styles.logo} src="/static/pugtatoes-logo.svg" alt="Pugtatoes" loading="eager"></img></Link>
+          <Link href='/'>
+            <img className={styles.logo} src="/static/pugtatoes-logo.svg" alt="Pugtatoes" loading="eager" />
+          </Link>
           <ul>
-            <Link href="/collections/new" style={{ textDecoration: "none", color: "inherit" }}><li className={styles.menu_item}>New</li></Link>
-            <li style={{ userSelect: "none", pointerEvents: "none" }}>|</li>
-            <Link href="/collections" style={{ textDecoration: "none", color: "inherit" }}><li className={styles.menu_item}>Shop All</li></Link>
-            <li style={{ userSelect: "none", pointerEvents: "none" }}>|</li>
-            <Link href="/collections" style={{ textDecoration: "none", color: "inherit" }}><li className={styles.menu_item}>Collections</li></Link>
-            <li style={{ userSelect: "none", pointerEvents: "none" }}>|</li>
-            <Link href="/pages/about" style={{ textDecoration: "none", color: "inherit" }}><li className={styles.menu_item}>About</li></Link>
+            <Link href="/collections/new">
+              <li className={styles.menu_item}>New</li>
+            </Link>
+            <li>|</li>
+            <Link href="/collections">
+              <li className={styles.menu_item}>Shop All</li>
+            </Link>
+            <li>|</li>
+            <Link href="/collections">
+              <li className={styles.menu_item}>Collections</li>
+              <div className={styles.chevron}>
+                <img src='/static/chevron-down.svg' alt="chevron down" />
+              </div>
+            </Link>
+            <li>|</li>
+            <Link href="/pages/about">
+              <li className={styles.menu_item}>About</li>
+              <div className={styles.chevron}>
+                <img src='/static/chevron-down.svg' alt="chevron down" />
+              </div>
+            </Link>
           </ul>
           <div className={styles.mobile_menu_icon_2}>
-            <img className={styles.nav_icon_2} src="/static/account.svg" alt="menu icon"></img>
-            <img className={styles.nav_icon} src="/static/search.svg" alt="menu icon"></img>
-            <img className={styles.nav_icon} src="/static/cart.svg" alt="menu icon"></img>
+            <img className={styles.nav_icon_2} src="/static/account.svg" alt="menu icon" />
+            <img className={styles.nav_icon} src="/static/search.svg" alt="menu icon" />
+            <img className={styles.nav_icon} src="/static/cart.svg" alt="menu icon" />
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
