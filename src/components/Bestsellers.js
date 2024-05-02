a04fa94835182a6c9f3356d0ceae03331f4afa7b
derkiz <aconejulien@gmail.com>
2024-05-02 18:08:32 +0200

    filters bestsellers collection only

--- src/components/Bestsellers.js
+++ src/components/Bestsellers.js
@@ -1,24 +1,23 @@
-// src/app/components/Latest.js
+// src/app/components/Bestsellers.js
 
-{/* debugging... to show url: <div>{process.env.STRAPI_APP_BASE_URL + product.attributes.image.data[0].attributes.url}</div> */}
-
-import React from 'react'
-import styles from './Bestsellers.module.css'
+import React from 'react';
+import styles from './Bestsellers.module.css';
 import Link from 'next/link';
 
 const Bestsellers = async () => {
   try {
     const response = await fetch('http://localhost:1337/api/products?populate=image');
     const { data } = await response.json();
-
+    // Filter bestsellers from data
+    const bestsellers = data.filter(product => product.attributes.collection === 'bestsellers');
     return (
       <div className={styles.flex_padding}>
         <div className={styles.container}>
           <div className={styles.title}>Meet our bestsellers</div>
           <div className={styles.card_container}>
-            {data.map(product => (
+            {bestsellers.map(product => (
               <Link className={styles.card} href={`/products/${product.attributes.slug}`} key={product.id}>
-                <img src={process.env.STRAPI_APP_BASE_URL + product.attributes.image.data[0].attributes.url}/>
+                <img src={process.env.STRAPI_APP_BASE_URL + product.attributes.image.data[0].attributes.url} alt={product.attributes.title}/>
                 <div className={styles.chead}>{product.attributes.title}</div>
                 <div className={styles.cdesc}>€{product.attributes.price}</div>
               </Link>
@@ -29,7 +28,7 @@ const Bestsellers = async () => {
     );
   } catch (error) {
     console.error('Error fetching data:', error);
-    return null; // Return null in case of an error to render nothing
+    return null;
   }
 };
 
