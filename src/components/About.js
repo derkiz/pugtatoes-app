59803f13e8da0aa7228b1cb8ea28745a6f1ae904
derkiz <aconejulien@gmail.com>
2024-08-21 19:55:30 +0200

    fixes made to about section

--- src/components/About.js
+++ src/components/About.js
@@ -6,26 +6,12 @@ const About = () => {
     <>
       <div className={styles.flex_container}>
         <div className={styles.container} style={{ display: 'flex', flexDirection:'row'}}>
-
-          <div className={styles.box_1}>
-          <div className={styles.title} style={{ fontSize: '3em'}}>
-              Our Story
-            </div>
-            <div clasName={styles.paragraph}>
-              Our mission is to create distinctive, well-designed art that brings 
-              personality and style to your space. Each piece is thoughtfully crafted, 
-              starting with hand-drawn designs by talented artists, then elevated 
-              through digital techniques to ensure high quality and originality. 
-              With a focus on sustainability and local craftsmanship, we aim to offer 
-              art that not only looks great but also resonates with a sense of 
-              purpose and creativity.
+          <div className={styles.box}>
+            <div className={styles.innerBox}>
+              Our mission is to create distinctive, well-designed 
+              art that brings personality and style to your space. 
             </div>
           </div>
-
-          <div className={styles.box_2}>
-            <img src="../static/mug-header.png" loading="eager"></img>
-          </div>
-
         </div>
       </div>
     </>
