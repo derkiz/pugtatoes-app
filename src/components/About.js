cf7ecc635b2cfb00bf03bdb1189d75d78f64b41b
derkiz <aconejulien@gmail.com>
2024-08-22 18:18:08 +0200

    fixes about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,36 @@ const About = () => {
   return (
     <>
       <div className={styles.flex_container}>
-        <div className={styles.container} style={{ display: 'flex', flexDirection:'row'}}>
+        <div className={styles.container}>
           <div className={styles.box}>
+            <img src="/static/about-section.png" alt="Background" />
             <div className={styles.innerBox}>
               Our mission is to create distinctive, well-designed 
               art that brings personality and style to your space. 
+            </div>
+          </div>
+          <div className={styles.title} style={{ marginBottom:'2rem', marginTop: '2rem'}}>Our values</div>
+          <div className={styles.value_points}>
+            <div className={styles.item}>
+              <img src='/static/membrane.png' loading='eager' alt="Membrane" />
+              <div className={styles.item_title}>Thoughtfully Sourced Materials</div>
+              <div className={styles.item_text}>
+                All of our prints are made using thoughtfully sourced, eco-conscious materials. 
+              </div>
+            </div>
+            <div className={styles.item}>
+              <img src='/static/porcelain.png' loading='eager' alt="Porcelain" />
+              <div className={styles.item_title}>Locally Made</div>
+              <div className={styles.item_text}>
+                We proudly support local artisans by creating our prints and designs right here in the community. 
+              </div>
+            </div>
+            <div className={styles.item}>
+              <img src='/static/graphic-design.png' loading='eager' alt="Graphic Design" />
+              <div className={styles.item_title}>From Hand To Digital</div>
+              <div className={styles.item_text}>
+                Each design begins as a hand-drawn illustration, carefully transformed into a high-quality digital print.
+              </div>
             </div>
           </div>
         </div>
