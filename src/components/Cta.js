2bc4b274b4b9351db67d252fae1cdc7e4df5bc59
derkiz <aconejulien@gmail.com>
2024-08-23 16:30:20 +0200

    modifies home page

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Cta.module.css'
 import Head from 'next/head'
+import Link from 'next/link'
 
 const Cta = () => {
   return ( 
@@ -10,14 +11,16 @@ const Cta = () => {
     </Head>
     <div className={styles.header_cta}>
       <div className={styles.element_1}>
-        <img className={styles.image_element} src="static/mug-header.png" loading="eager"></img>
+        <img className={styles.image_element} src="static/header.png" loading="eager"></img>
       </div>
       <div className={styles.element_2}>
         <div className={styles.element_main}>
-          <div className={styles.title}>Take a sip</div>
+          <div className={styles.title}>Explore our collections!</div>
           <div className={styles.p_elem}>A great gift, a cute collectible, a chubby pug, a silly companion. See our latest festive designs! :-)</div>
           <div className={styles.cta_btn}>
-            Pick out a mug!
+            <Link href="/collections">
+              <div>Collections</div>
+            </Link>
           </div>
         </div>
       </div>
