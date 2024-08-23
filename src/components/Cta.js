3aa2a9239f12d01093a62a2467f38cbfcd9fc447
derkiz <aconejulien@gmail.com>
2024-08-23 20:10:58 +0200

    small fixes to search bar

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -16,10 +16,10 @@ const Cta = () => {
       <div className={styles.element_2}>
         <div className={styles.element_main}>
           <div className={styles.title}>Explore our collections!</div>
-          <div className={styles.p_elem}>A great gift, a cute collectible, a chubby pug, a silly companion. See our latest festive designs! :-)</div>
+          <div className={styles.p_elem} style={{ textAlign:'center' }}>A great gift, a cute collectible, a chubby pug, a silly companion. See our latest festive designs! :-)</div>
           <div className={styles.cta_btn}>
             <Link href="/collections">
-              <div>Collections</div>
+              <div>Shop All</div>
             </Link>
           </div>
         </div>
