19cae8a49ead53e255954e40d39c9c516be791b0
derkiz <aconejulien@gmail.com>
2024-09-26 20:12:46 +0200

    adds favicon.ico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Cta, Bestsellers } from "@/components";
 import { CartProvider } from "@/contexts/CartContext"; // Ensure the path matches your project structure
 
 const Page = () => {
-  const STRAPI_APP_BASE_URL = process.env.NEXT_PUBLIC_STRAPI_APP_BASE_URL || 'failed';
 
   return (
     <CartProvider>
